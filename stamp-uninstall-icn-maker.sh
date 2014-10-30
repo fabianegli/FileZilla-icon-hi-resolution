1becd2fecf34ada0d6f1a19f751bc84bd6dbf496
fabianegli <fabian.egli@biol.ethz.ch>
2014-10-30 19:36:56 +0100

    Stamp version of the uninstall logo with seperate small svg. Updated .gitignore.

--- stamp-uninstall-icn-maker.sh
+++ stamp-uninstall-icn-maker.sh
@@ -15,14 +15,14 @@ cp -r FileZilla_stamp.iconset/ UninstallStampUninstallFileZilla.iconset
 
 # Define wich size should have which source svg 
 ### low resolution
-inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_16x16.png --export-width=16 --export-height=16
-inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_32x32.png --export-width=32 --export-height=32
+inkscape FileZilla_stamp_small.svg --export-png=StampUninstallFileZilla.iconset/icon_16x16.png --export-width=16 --export-height=16
+inkscape FileZilla_stamp_small.svg --export-png=StampUninstallFileZilla.iconset/icon_32x32.png --export-width=32 --export-height=32
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_128x128.png --export-width=128 --export-height=128
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_256x256.png --export-width=256 --export-height=256
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_512x512.png --export-width=512 --export-height=512
 ### high resolution
-inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_16x16@2x.png --export-width=32 --export-height=32
-inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_32x32@2x.png --export-width=64 --export-height=64
+inkscape FileZilla_stamp_small.svg --export-png=StampUninstallFileZilla.iconset/icon_16x16@2x.png --export-width=32 --export-height=32
+inkscape FileZilla_stamp_small.svg --export-png=StampUninstallFileZilla.iconset/icon_32x32@2x.png --export-width=64 --export-height=64
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_128x128@2x.png --export-width=256 --export-height=256
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_256x256@2x.png --export-width=512 --export-height=512
 inkscape FileZilla_stamp.svg --export-png=StampUninstallFileZilla.iconset/icon_512x512@2x.png --export-width=1024 --export-height=1024
